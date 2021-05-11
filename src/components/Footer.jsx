524a835394c7e647867ef285d91dfb1cc1979219
Harrison Malone <harrison.malone@hey.com>
2021-05-11 13:05:33 +1000

    Add https url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export function Footer() {
         Archive
       </a>
       <a
-        href="http://github.com/harrisonmalone"
+        href="https://github.com/harrisonmalone"
         style={{
           marginRight: "10px",
         }}
